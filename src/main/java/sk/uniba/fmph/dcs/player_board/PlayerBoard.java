26a343a1cc66d9456bbad6d98d3ba077ba129ab3
Maksym <maxpavliy@gmail.com>
2024-11-20 10:21:12 +0100

    PlayerBoard prep + clean

--- src/main/java/sk/uniba/fmph/dcs/player_board/PlayerBoard.java
+++ src/main/java/sk/uniba/fmph/dcs/player_board/PlayerBoard.java
@@ -2,28 +2,41 @@ package sk.uniba.fmph.dcs.player_board;
 
 import java.util.*;
 
-public class PlayerBoard {
+import sk.uniba.fmph.dcs.stone_age.InterfaceGetState;
+
+public class PlayerBoard implements InterfaceGetState {
     private int points;
     private int houses;
-    private final int playerFigures;
+//    private final playerFigures;
+//    private final playerTools;
+//    private final feedTribe;
+    private boolean endGamePoints;
+
 
     public PlayerBoard() {
 
     }
 
-    public void addPoints(int points) {
+    //Adding some number to users total points
+    //return new amount of points
+    public int addPoints(int points) {
         this.points += points;
+        return this.points;
     }
 
+    //Incrementing amount of houses user has
     public void addHouse() {
-
+        this.houses++;
     }
 
     public void addEndGamePoints() {
 
     }
 
+    //Showing the current state
+    @Override
     public String state() {
-        return "";
+        return "Current points: " + points + "\n" +
+                "Current houses: " + houses + "\n";
     }
 }
