60563701c88f900e86cb71c438dd270d04862cba
Vladyslav Pehushyn <vlad.pegushyn1@gmail.com>
2024-11-22 10:49:54 +0100

    implement class Throw

--- src/main/java/sk/uniba/fmph/dcs/game_board/Throw.java
+++ src/main/java/sk/uniba/fmph/dcs/game_board/Throw.java
@@ -1,8 +1,15 @@
 package sk.uniba.fmph.dcs.game_board;
 
+import java.util.Random;
+
 public class Throw {
 
-    /*private int[] throw(int dices){
-
-    }*/
+    public int[] throw_(int dices){
+        int[] result = new int[dices];
+        Random rand = new Random();
+        for (int i = 0; i < dices; i++) {
+            result[i] = rand.nextInt(6)+1;
+        }
+        return result;
+    }
 }
