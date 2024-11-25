4905e1f0896d8edb3a495696177b0100f3050536
Vladyslav Pehushyn <vlad.pegushyn1@gmail.com>
2024-11-25 14:41:27 +0100

    prepared to update

--- src/main/java/sk/uniba/fmph/dcs/game_board/CurrentThrow.java
+++ src/main/java/sk/uniba/fmph/dcs/game_board/CurrentThrow.java
@@ -1,30 +1,50 @@
 package sk.uniba.fmph.dcs.game_board;
 
-import sk.uniba.fmph.dcs.stone_age.Effect;
-import sk.uniba.fmph.dcs.stone_age.InterfacePlayerBoardGameBoard;
-import sk.uniba.fmph.dcs.stone_age.InterfaceToolUse;
-import sk.uniba.fmph.dcs.stone_age.Player;
+import sk.uniba.fmph.dcs.stone_age.*;
 
 public class CurrentThrow implements InterfaceToolUse {
     private Effect throwsFor;
     private int throwResult;
     private Player player;
     private int dices;
-    private InterfacePlayerBoardGameBoard playerBoard;
+    private PlayerBoard playerBoard;
+    private int sumScoreOfDices;
 
 
     public CurrentThrow(Player player, Effect effect, int dices){
         this.player = player;
         throwsFor = effect;
         this.dices = dices;
+        this.playerBoard = this.player.playerBoard();
+        sumScoreOfDices = sumDices();
+    }
+
+    private int sumDices(){
+        Throw throw_ = new Throw();
+        int[] score = throw_.throw_(dices);
+        int finalScore = 0;
+        for (int i = 0; i < score.length; i++) {
+            finalScore += score[i];
+        }
+
+        return finalScore;
     }
 
     public String state(){
+
+        switch (throwsFor){
+            case FOOD:
+
+                break;
+        }
+
         return "";
     }
 
     @Override
     public boolean useTool(int idx) {
+        playerBoard.useTool(idx);
+
         return false;
     }
 
