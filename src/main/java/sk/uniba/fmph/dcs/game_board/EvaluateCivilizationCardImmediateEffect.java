774d7185ad16b4c4c6abcc6518d84d5100620ef9
Arsenii-Nemchenko <arsenii.nemchenko@gmail.com>
2024-11-28 13:54:19 +0200

    Added getCard, immediateEffect
    Added getCard, changed Effect

---
+++ src/main/java/sk/uniba/fmph/dcs/game_board/EvaluateCivilizationCardImmediateEffect.java
@@ -0,0 +1,10 @@
+package sk.uniba.fmph.dcs.game_board;
+
+import sk.uniba.fmph.dcs.stone_age.Effect;
+import sk.uniba.fmph.dcs.stone_age.Player;
+import sk.uniba.fmph.dcs.stone_age.ImmediateEffect;
+public interface EvaluateCivilizationCardImmediateEffect {
+    boolean performEffect(Player player, Effect choice);
+
+
+}
---
+++ src/main/java/sk/uniba/fmph/dcs/game_board/GetCard.java
@@ -0,0 +1,25 @@
+package sk.uniba.fmph.dcs.game_board;
+
+import sk.uniba.fmph.dcs.stone_age.Effect;
+import sk.uniba.fmph.dcs.stone_age.Player;
+
+import java.util.Optional;
+
+public class GetCard implements  EvaluateCivilizationCardImmediateEffect{
+    private CivilizationCardDeck deck;
+
+    public GetCard(CivilizationCardDeck deck){
+        this.deck = deck;
+    }
+    @Override
+    public boolean performEffect(Player player, Effect effect){
+        Optional<CivilizationCard> card = deck.getTop();
+        if(card.isPresent()) {
+            player.playerBoard().giveCard(card.get());
+            return true;
+        }else{
+            return false;
+        }
+
+    }
+}
