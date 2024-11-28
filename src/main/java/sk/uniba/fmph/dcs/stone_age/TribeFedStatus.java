e31c3a785257c934878f4b7651635f6cdafdbda7
Vladyslav Havriuk <web.user@ukr.net>
2024-11-28 16:33:08 +0100

    Buildingtile etc

--- src/main/java/sk/uniba/fmph/dcs/stone_age/TribeFedStatus.java
+++ src/main/java/sk/uniba/fmph/dcs/stone_age/TribeFedStatus.java
@@ -3,6 +3,7 @@ package sk.uniba.fmph.dcs.stone_age;
 import org.json.JSONObject;
 import java.util.Collection;
 import java.util.Map;
+import sk.uniba.fmph.dcs.player_board.PlayerFigures;
 
 public final class TribeFedStatus implements InterfaceFeedTribe, InterfaceNewTurn {
     private static final int MAX_FIELDS = 10;
