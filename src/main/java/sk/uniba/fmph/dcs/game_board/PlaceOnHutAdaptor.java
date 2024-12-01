21381d274f301c2b3a546708e32f2dd7ec299c90
Vladyslav Pehushyn <vlad.pegushyn1@gmail.com>
2024-12-01 17:45:58 +0100

    implemented PlaceOnHutAdaptor

--- src/main/java/sk/uniba/fmph/dcs/game_board/PlaceOnHutAdaptor.java
+++ src/main/java/sk/uniba/fmph/dcs/game_board/PlaceOnHutAdaptor.java
@@ -7,23 +7,42 @@ import sk.uniba.fmph.dcs.stone_age.HasAction;
 import sk.uniba.fmph.dcs.stone_age.Player;
 
 public class PlaceOnHutAdaptor implements InterfaceFigureLocationInternal{
-    /*private ToolMakerHutFields toolMakerHutFields;
+    private ToolMakerHutFields toolMakerHutFields;
 
     public PlaceOnHutAdaptor(ToolMakerHutFields toolMakerHutFields){
         this.toolMakerHutFields = toolMakerHutFields;
-    }*/
+    }
     @Override
     public boolean placeFigures(Player player, int figureCount) {
+        if(figureCount >= 1 && toolMakerHutFields.canPlaceOnHut(player)){
+            toolMakerHutFields.placeOnHut(player);
+            return true;
+        }
         return false;
     }
 
     @Override
     public HasAction tryToPlaceFigures(Player player, int count) {
-        return ActionResult.FAILURE;
+        if(!player.playerBoard().hasFigures(count)){
+            return HasAction.NO_ACTION_POSSIBLE;
+        }
+        if(toolMakerHutFields.canPlaceOnFields(player)){
+            return HasAction.WAITING_FOR_PLAYER_ACTION;
+        }
+
+        return HasAction.NO_ACTION_POSSIBLE;
     }
 
     @Override
     public ActionResult makeAction(Player player, Effect[] inputResources, Effect[] outputResources) {
+        if(!toolMakerHutFields.canPlaceOnToolMaker(player)){
+            return ActionResult.FAILURE;
+        }
+
+        if(toolMakerHutFields.actionHut(player)) {
+            return ActionResult.ACTION_DONE;
+        }
+
         return ActionResult.FAILURE;
     }
 
@@ -34,7 +53,11 @@ public class PlaceOnHutAdaptor implements InterfaceFigureLocationInternal{
 
     @Override
     public HasAction tryToMakeAction(Player player) {
-        return ActionResult.FAILURE;
+        if(toolMakerHutFields.canPlaceOnHut(player)){
+            return HasAction.WAITING_FOR_PLAYER_ACTION;
+        }
+
+        return HasAction.NO_ACTION_POSSIBLE;
     }
 
     @Override
